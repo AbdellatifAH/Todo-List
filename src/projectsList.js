604e0fe308f38f93a9ba0f5f5329bdd9f4244d73
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-12-13 17:26:08 +0200

    Added a new allTasks array. Added defaultProject Object. Added updateIndex function to update the index stored inside of tasks and projects. Added dropTaskFromAllTasks function. Modified findProject function to only find the project. Modified addNewProjectObject function, now it will check if the project exists first before creating a new project. Modified dropTask function, now it utilize updateIndex() after dropping a task. Modified dropProject function, now it utilize updateIndex() after dropping a project.

--- src/projectsList.js
+++ src/projectsList.js
@@ -1,49 +1,73 @@
 import Project from './projects';
 import Task from './tasks';
 export const allProjects = [];
+export const allTasks = [];
+export const defaultProject = Project("General Tasks");
 
 export function addNewTaskObject(taskName, description, dueDate, priority, status, projectName) {
     const newTask = Task(taskName, description, dueDate, priority, status, projectName);
-    const project = findProject(projectName);
-    linkTaskAndProject(project, newTask);
+    allTasks.push(newTask);
+    newTask.indexInAllTasks = allTasks.length - 1;
+    let project;
+    if (projectName) {
+        project = addNewProjectObject(projectName);
+        linkTaskAndProject(project, newTask);
+    }
+    else
+        linkTaskAndProject(defaultProject, newTask);
     return newTask;
 }
 
-function findProject(projectName) {
+export function findProject(projectName) {
 
-    if (projectName == "")
-        projectName = "defaultProject";
-
-    let project = allProjects.find((projectInstance) => {
+    const project = allProjects.find((projectInstance) => {
         if (projectInstance.name == projectName)
             return projectInstance
     })
 
-    if (project === undefined)
-        project = addNewProjectObject(projectName)
     return project;
 }
 
 export function addNewProjectObject(projectName) {
-    const newProject = Project(projectName);
-    allProjects.push(newProject);
-    newProject.indexInAllProject = allProjects.length - 1;
+    let newProject = findProject(projectName);
+    if (newProject === undefined) {
+        newProject = Project(projectName)
+        allProjects.push(newProject);
+        newProject.indexInAllProject = allProjects.length - 1;
+    }
     return newProject;
 }
 
 function linkTaskAndProject(project, task) {
     task.project = project;
-    (project.tasksList).push(task);
+    project.tasksList.push(task);
     task.indexInProjectTasksList = task.project.tasksList.length - 1;
 }
 
+function updateIndex(array, indexName) {
+    array.forEach((instance, index) => {
+        instance[indexName] = index;
+    })
+}
+
+function dropTaskFromAllTasks(task) {
+    allTasks.splice(task.indexInAllTasks, 1);
+    updateIndex(allTasks, 'indexInAllTasks');
+}
+
 export function dropTask(task) {
-    task.project.tasksList.splice(task.indexInProjectTasksList);
+    allTasks.splice(task.indexInAllTasks, 1);
+    updateIndex(allTasks, 'indexInAllTasks');
+
+    task.project.tasksList.splice(task.indexInProjectTasksList, 1);
+    updateIndex(task.project.tasksList, 'indexInProjectTasksList');
+    task.project = "";
 }
 
 export function dropProject(project) {
-    project.tasksList.forEach(task => dropTask(task));
-    allProjects.splice(project.indexInAllProject);
+    project.tasksList.forEach(task => dropTaskFromAllTasks(task));
+    allProjects.splice(project.indexInAllProject, 1);
+    updateIndex(allProjects, 'indexInAllProject');
 }
 
 export function createProjectsList() {
